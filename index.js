926a95cf0797df697d5c508b14aa05ad2d7b8e48
Sushant1-2 <sanjay9829133956@gmail.com>
2025-08-11 19:13:37 +0545

    old

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const cors = require("cors");
 const app = express();
-
-require("./db.js");
-
+const db = require("./db.js");
+const cors = require("cors");
 const productRoute = require("./routes/productRoute.js");
 const orderRoute = require("./routes/orderRoutes.js");
 const userRoute = require("./routes/userRoute.js");
 
-const allowedOrigin = "https://clever-mousse-3bdc0a.netlify.app";
-
 app.use(express.json());
+app.use(cors());
 
-app.use(
-  cors({
-    origin: allowedOrigin,
-    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
-
-app.options(
-  "*",
-  cors({
-    origin: allowedOrigin,
-    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello from e-commerce project");
@@ -38,8 +20,6 @@ app.get("/", (req, res) => {
 app.use("/product", productRoute);
 app.use("/order", orderRoute);
 app.use("/user", userRoute);
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Application running on port", PORT);
